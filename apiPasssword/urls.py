4fa4f38bf9859d510db43e8f4e07fad1e78daa91
krlltch2909 <angron2002@mail.ru>
2022-07-06 18:14:38 +0300

    new branch

--- apiPasssword/urls.py
+++ apiPasssword/urls.py
@@ -2,18 +2,20 @@ from django.urls import path, include, re_path
 from .views import *
 from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register(r'password', PasswordViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'password', PasswordViewSet)
 
 
 router_user = routers.DefaultRouter()
 router_user.register(r'user', UserViewSet, basename="user")
-for i in router.urls:
-    print(i)
+# for i in router.urls:
+#     print(i)
 
 urlpatterns = [
     path('get/', include(router_user.urls)),    # working function, for passwords for id user
-    path('get/', include(router.urls)),         # main function
+    path('add/', AddPassword.as_view()),
+    path('delite_password/<int:pk>/', DelitePassword.as_view()),
+    # path('get/', include(router.urls)),         # main function
     path('get/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
--- apiPasssword/views.py
+++ apiPasssword/views.py
@@ -2,8 +2,9 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import UserSerializer, PasswordSerializer
 from main.models import Password, AccauntUser
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from rest_framework.decorators import action
+from .permissions import IsOwnerOrAdmin
 
 
 # Create your views here.
@@ -11,10 +12,21 @@ from rest_framework.decorators import action
 # улучшенная версия верхних классов
 
 
-class PasswordViewSet(viewsets.ModelViewSet):
-    queryset = Password.objects.all()
+class DelitePassword(generics.DestroyAPIView):
+    permission_classes = [IsOwnerOrAdmin,]
     serializer_class = PasswordSerializer
-    permission_classes = [IsAdminUser]
+    queryset = Password.objects.all()
+
+
+class AddPassword(generics.ListCreateAPIView):
+    serializer_class = PasswordSerializer
+    queryset = Password.objects.all()
+
+
+# class PasswordViewSet(viewsets.ModelViewSet):
+#     queryset = Password.objects.all()
+#     serializer_class = PasswordSerializer
+#     permission_classes = [IsAdminUser]
 
 
 class UserViewSet(viewsets.ModelViewSet):
